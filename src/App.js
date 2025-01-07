a3b931004167e908637332e16d13b6d7ca5d473c
chrisIyer <hellohotbox420@gmail.com>
2025-01-07 11:46:55 -0500

    reran this app. It is working fine now.  I am going to commit the changes. I would really like to take out the front end mongoose connection, nd get python to do all that on the backend.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import CreateUserForm from "./CreateUserForm.js";
- import ListUsersToEdit from "./ListUsersToEdit.js";
+import ListUsersToEdit from "./ListUsersToEdit.js";
 const App = () => {
   return (
     <div>
       <h1>Create User</h1>
       <CreateUserForm />
- <ListUsersToEdit/> 
+      <ListUsersToEdit />
 
     </div>
   );
